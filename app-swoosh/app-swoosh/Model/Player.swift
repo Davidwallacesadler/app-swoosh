0072b56be4ea34377891eca91cfdcf851e725e87
David Sadler <davidwallacesadler@gmail.com>
2018-10-28 15:58:07 -0600

    Add IBAction logic and Model for data

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by David Sadler on 10/28/18.
+//  Copyright © 2018 David Sadler. All rights reserved.
+//
+
+import Foundation
+
+//always use a struct when you can - using default swift types. use classes for custom implementations of types. Structs are faster.
+//all data in the app should be dealt with in a class or a struct - handled with the model
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
+//This model is for handling the data we want to pass between VC's
--- app-swoosh/app-swoosh/New Group/LeagueVC.swift
+++ app-swoosh/app-swoosh/New Group/LeagueVC.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    //create an instance of player in the leagueVC
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //Initalize the player Model when viewDidLoad is called
+        player = Player()
         }
+    
+    //withing each IBAction we want to specify the logic we need to pass Model data between VC's
+    //never have logic built into the action itself - define the logic in the VC scope
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
   
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
     
 
-
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+       selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
 }
